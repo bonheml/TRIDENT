6414d61d984184219fccb5b6407b8c6d73aba3bd
Lisa Bonheme <bonhemel@iarc.who.int>
2025-08-14 16:14:28 +0200

    unfreeze slide encoder layers

--- trident/wsi_objects/WSI.py
+++ trident/wsi_objects/WSI.py
@@ -1056,7 +1056,7 @@ class WSI:
         else:
             # Generate slide-level relevancy scores
             with torch.autocast(device_type='cuda', enabled=(slide_encoder.precision != torch.float32)):
-                relevancy_scores = attn_grad_rollout(batch, weights, device)
+                relevancy_scores = attn_grad_rollout(batch, weights, device=device)
         relevancy_scores = relevancy_scores.cpu().numpy()
         print(relevancy_scores.shape)
 
